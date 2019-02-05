f5811f3438cfec43b431aa2a78b902d378fda040
rc0033939 <rc0033939@CRO-24-17>
2019-02-05 10:26:58 +0000

    MOVE up right left down

--- move picture/Form1.cs
+++ move picture/Form1.cs
@@ -21,5 +21,20 @@ namespace move_picture
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X , pictureBox1.Location.Y - 5);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X , pictureBox1.Location.Y + 5);
+        }
     }
 }
